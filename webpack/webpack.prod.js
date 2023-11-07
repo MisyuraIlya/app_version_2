540ccbcf9773c553177df555f06f471d2b9d9b46
Ilya misyura <ilya.mi@digitrade.co.il>
2023-11-08 00:13:08 +0200

    solve deploy

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,39 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: false,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+			filename: 'main.css'
+		}),
     new webpack.DefinePlugin({
       'process.env.name': JSON.stringify('Codevolution'),
     }),
   ],
+
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 }
