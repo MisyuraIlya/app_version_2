5ecb13080927999fba1fbbc2b29060990b03d029
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2023-11-07 17:54:09 +0200

    add configs to webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { useState } from 'react';
+import './styles/main.css'
 
 function App() {
+  const [count, setCounter] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
-        Hello world
+        <h1>Hello world {process.env.NODE_ENV} {process.env.name}</h1>
+        <button onClick={() => setCounter(c => c+ 1)}>{count}</button>
       </header>
     </div>
   );
