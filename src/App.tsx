19e18ed1e023a5eca79bfad273c56c54bd6a84cb
Ilya misyura <ilya.mi@digitrade.co.il>
2023-11-07 21:24:07 +0200

    test3

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import './styles/main.css'
 
 function App() {
   const [count, setCounter] = useState(0)
-  const name = "as"
+  const name = 'as'
   return (
     <div className="App">
       <header className="App-header">
         <h1>
-          Hello world {process.env.NODE_ENV} {process.env.name}
+          Hello world {process.env.NODE_ENV} {process.env.name} {name}
         </h1>
         <button onClick={() => setCounter((c) => c + 1)}>{count}</button>
       </header>
