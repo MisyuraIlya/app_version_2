be42df68f8f6ff60075097d4442302eef3a5e182
Ilya misyura <ilya.mi@digitrade.co.il>
2023-11-07 21:07:38 +0200

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import './styles/main.css'
 
 function App() {
   const [count, setCounter] = useState(0)
-  const name = 'asdas'
+  const name = 'asdaa'
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world {process.env.NODE_ENV} {process.env.name}</h1>
-        <button onClick={() => setCounter(c => c+ 1)}>{count}</button>
+        <h1>
+          Hello world {process.env.NODE_ENV} {process.env.name}
+        </h1>
+        <button onClick={() => setCounter((c) => c + 1)}>{count}</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
